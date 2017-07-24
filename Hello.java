1357a142b6ac302f3c31612d7e7b125a3c0be965
LzyCodebase <30403576+LzyCodebase@users.noreply.github.com>
2017-07-24 16:26:35 +0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+package com.briup.day01;
+
+import java.util.Date;
+
+public class Hello {
+	public static void main (String[] args){
+		Date now=new Date();
+		System.out.println("hello world");
+		System.out.println(now);
+	}
+}
